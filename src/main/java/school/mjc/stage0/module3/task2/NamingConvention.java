34a4196cb85b75305ddac64114e2c1cc08301b55
lownn <kurilyakoleksii12@gmail.com>
2024-07-11 23:51:43 +0200

    task2

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable_ten = 10;
-        int variable_one = 1;
-        int variable_two = 2;
-        int variable_three = 3;
-        final int variable_four = 4;
-        final int variable_five = 5;
-        int variable_six = 6;
-        int variable_seven = 7;
-        int variable_eight = 8;
+        int variableTen = 10;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
